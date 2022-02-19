f4d7bec6299de13314fe577777725282da397680
jvson03 <xdropyt@gmail.com>
2022-02-19 19:29:57 +0100

    Fixes

--- client/main.lua
+++ client/main.lua
@@ -1,14 +1,22 @@
-RegisterNetEvent('dotsoft_popup:Pop')
-AddEventHandler('dotsoft_popup:Pop', function(text)
+function Pop(message)
 	SendNUIMessage({
 		action = 'pop',
-		text = text,
+		text = message,
 	})
+end
+
+function Hide()
+	SendNUIMessage({
+		action = 'hide'
+	})
+end
+
+RegisterNetEvent('dotsoft_popup:Pop')
+AddEventHandler('dotsoft_popup:Pop', function(message)
+	Pop(message)
 end)
 
 RegisterNetEvent('dotsoft_popup:Hide')
 AddEventHandler('dotsoft_popup:Hide', function()
-	SendNUIMessage({
-		action = 'hide'
-	})
+	Hide()
 end)
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,16 +5,12 @@ author 'DotSoft | JasonTheMan#7125'
 description 'Dotsoft PopUp'
 version '1.0'
 
-ui_page {
-    'web/popup.html',
-}
-
-files {
-	'web/popup.html',
-	'web/js/popup.js', 
-	'web/css/popup.css',
-}
+ui_page 'web/popup.html'
 
 client_scripts {
 	'client/main.lua'
+}
+
+files {
+	'web/*.*'
 }
